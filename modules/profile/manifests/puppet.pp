052d75006881d23989d1e1e0cce281af5b622681
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-10-25 16:33:17 +1300

    use puppet::conf do manage puppet.conf
    use puppet::hiera to add a default hiera configuration, even though this isn't used by a puppet agent it suppresses a warning.
    
    Tuned .gitignore for the changes in the puppet install.

--- modules/profile/manifests/puppet.pp
+++ modules/profile/manifests/puppet.pp
@@ -1,6 +1,9 @@
 # This role installs the puppet agent
 class profile::puppet {
   class{'::puppet':
+    ensure  => '3.3.1-1puppetlabs1',
     require => Package['ruby'],
   }
+  include puppet::conf
+  include puppet::hiera
 }
