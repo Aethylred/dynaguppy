b4ba238bfdf3c3be6b669b192e7f761df2dbfab8
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-10-30 12:36:28 +1300

    Using the brightbox Ruby repository does not solve all the issues.

--- modules/profile/manifests/repositories.pp
+++ modules/profile/manifests/repositories.pp
@@ -5,14 +5,12 @@ class profile::repositories {
         purge_sources_list_d  => true,
       }
       include repository::puppetlabs
-      include repository::ruby
     }
     RedHat:{
       Yumrepo{
         stage => 'prep_repos',
       }
       include repository::puppetlabs
-      include repository::ruby
     }
     default:{
       warning("There are no repositories configured for ${::osfamily}")
--- modules/profile/manifests/ruby/puppet.pp
+++ modules/profile/manifests/ruby/puppet.pp
@@ -4,5 +4,7 @@ class profile::ruby::puppet {
   # This installs the default ruby package
   # ...a repository with a later ruby version installed will
   # be required to get Ruby 1.9.3 or 2.x
-  include ruby
+  class{'ruby':
+   # version => '1.8.7',
+  }
 }
