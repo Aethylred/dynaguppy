65e39b88f103b3b58c148ceb91227422b550ba27
Aaron Hicks <hicksa@landcareresearch.co.nz>
2014-11-07 14:28:14 +1300

    update app and shell versions

--- environments/production/modules/profile/manifests/gitlab.pp
+++ environments/production/modules/profile/manifests/gitlab.pp
@@ -7,10 +7,14 @@ class profile::gitlab {
   }
 
   class{'::gitlab':
-    gitlab_url      => "http://${::gitlab_host}/",
-    gitlab_app_repo => 'https://github.com/gitlabhq/gitlabhq.git',
-    gitlab_app_rev  => 'v7.4.3',
-    require         => [
+    gitlab_url        => "http://${::gitlab_host}/",
+    enable_https      => true,
+    redirect_http     => true,
+    gitlab_app_repo   => 'https://github.com/gitlabhq/gitlabhq.git',
+    gitlab_app_rev    => '7-5-stable',
+    gitlab_shell_repo => 'https://github.com/gitlabhq/gitlab-shell.git',
+    gitlab_shell_rev  => 'v2.2.0',
+    require           => [
       Package[
         $dep_packages
       ]
