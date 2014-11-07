d71766e6ac48f4fd058d4298415701e0f2adfcb2
Aaron Hicks <hicksa@landcareresearch.co.nz>
2014-11-07 14:24:50 +1300

    Working on automaitng GitLab git hook scripts

--- environments/production/modules/profile/manifests/gitlab.pp
+++ environments/production/modules/profile/manifests/gitlab.pp
@@ -7,10 +7,12 @@ class profile::gitlab {
   }
 
   class{'::gitlab':
-    gitlab_url      => "http://${::gitlab_host}/",
-    gitlab_app_repo => 'https://github.com/gitlabhq/gitlabhq.git',
-    gitlab_app_rev  => 'v7.4.3',
-    require         => [
+    gitlab_url        => "http://${::gitlab_host}/",
+    gitlab_app_repo   => 'https://github.com/gitlabhq/gitlabhq.git',
+    gitlab_app_rev    => 'v7.4.3',
+    gitlab_shell_repo => 'https://github.com/Aethylred/gitlab-shell.git',
+    gitlab_shell_rev  => 'feature/custom_hooks',
+    require           => [
       Package[
         $dep_packages
       ]
