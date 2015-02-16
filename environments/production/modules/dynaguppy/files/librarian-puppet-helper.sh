5aa4033d47da4f6f999126f5c4ed6beec9cf706f
Aaron Hicks <hicksa@landcareresearch.co.nz>
2015-02-17 12:12:06 +1300

    Fix librarian-puppet checks. Should check for missing before checking if update required.

--- environments/production/modules/dynaguppy/files/librarian-puppet-helper.sh
+++ environments/production/modules/dynaguppy/files/librarian-puppet-helper.sh
@@ -5,40 +5,42 @@
 # it completes successfully.
 # 
 # This script should be run in a puppet environment directory
-
-echo 'Checking Puppetfile status'
-if git show --pretty --no-color --name-only | grep Puppetfile > /dev/null 2>&1 ; then
-  echo 'Puppetfile updated in last commit'
-  if test -d 'library' ; then
-    echo 'Dynaguppy module library exists'
+if test -d 'library' ; then
+  echo 'Dynaguppy module library exists'
+  echo 'Checking Puppetfile status'
+  if git show --pretty --no-color --name-only | grep Puppetfile > /dev/null 2>&1 ; then
+    echo 'Puppetfile updated in last commit'
     logger -isd -p syslog.info -t DYNAGUPPY 'Attempting to update Dynaguppy Puppet module library'
     until output=$(librarian-puppet update 2>&1) ; do
       # If error was captured some fixes could go here...
       if echo $output | grep 'Could not resolve the dependencies.' ; then
         logger -isd -p syslog.error -t DYNAGUPPY 'Updating Dynaguppy Puppet module library failed, could not resolve the dependencies. Exiting.'
         break
+      elif echo $output | grep 'Unable to find module' ; then
+        logger -isd -p syslog.error -t DYNAGUPPY 'Updating Dynaguppy Puppet module library failed, could not find new modules. Exiting.'
+        break
       fi
       logger -isd -p syslog.warning -t DYNAGUPPY 'Updating Dynaguppy Puppet module library failed, retrying'
     done
     logger -isd -p syslog.info -t DYNAGUPPY 'Dynaguppy Puppet module library update completed'
   else
-    echo 'Dynaguppy module library missing'
-    logger -isd -p syslog.info -t DYNAGUPPY 'Attempting to initialise Dynaguppy Puppet module library'
-    until output=$(librarian-puppet install --clean 2>&1) ; do
-      # If error was captured some fixes could go here...
-      if echo $output | grep 'Could not resolve the dependencies.' ; then
-        logger -isd -p syslog.error -t DYNAGUPPY 'Initialising Dynaguppy Puppet module library failed, could not resolve the dependencies. Exiting.'
-        break
-      elif echo $output | grep 'Unable to find module' ; then
-        logger -isd -p syslog.error -t DYNAGUPPY 'Updating Dynaguppy Puppet module library failed, could not find new modules. Exiting.'
-        break
-      fi
-      logger -isd -p syslog.warning -t DYNAGUPPY 'Initialising Dynaguppy Puppet module library failed, retrying'
-    done
-    logger -isd -p syslog.info -t DYNAGUPPY 'Dynaguppy Puppet module library initialisation completed'
+    logger -isd -p syslog.info -t DYNAGUPPY 'Puppetfile not updated in last commit, skipping librarian-puppet'
   fi
 else
-  logger -isd -p syslog.info -t DYNAGUPPY 'Puppetfile not updated in last commit, skipping librarian-puppet'
+  echo 'Dynaguppy module library missing'
+  logger -isd -p syslog.info -t DYNAGUPPY 'Attempting to initialise Dynaguppy Puppet module library'
+  until output=$(librarian-puppet install --clean 2>&1) ; do
+    # If error was captured some fixes could go here...
+    if echo $output | grep 'Could not resolve the dependencies.' ; then
+      logger -isd -p syslog.error -t DYNAGUPPY 'Updating Dynaguppy Puppet module library failed, could not resolve the dependencies. Exiting.'
+      break
+    elif echo $output | grep 'Unable to find module' ; then
+      logger -isd -p syslog.error -t DYNAGUPPY 'Updating Dynaguppy Puppet module library failed, could not find new modules. Exiting.'
+      break
+    fi
+    logger -isd -p syslog.warning -t DYNAGUPPY 'Initialising Dynaguppy Puppet module library failed, retrying'
+  done
+  logger -isd -p syslog.info -t DYNAGUPPY 'Dynaguppy Puppet module library initialisation completed'
 fi
 
 exit $?
